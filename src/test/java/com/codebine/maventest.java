b4545aaf4acea2751dbdee84f65bd15f7e528a40
vinod <vinod.tyagi222@gmail.com>
2019-10-11 10:55:23 +0530

    commit for maven project

---
+++ src/test/java/com/codebine/maventest.java
@@ -0,0 +1,14 @@
+package com.codebine;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class maventest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
